fd19e88086946e7182783afb8c7bcec4b8ce2d71
Steven Meyers <sjjmeyers@gmail.com>
2020-02-26 10:41:43 -0700

    Add to stock item test; pass

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -38,14 +38,10 @@ class VendorTest < Minitest::Test
     expected = {@item1 => 30}
 
     assert_equal expected, @vendor.inventory
+    @vendor.stock(@item1, 25)
+    @vendor.stock(@item2, 12)
+
+    expected = {@item1 => 55, @item2 => 12}
+    assert_equal expected, @vendor.inventory
   end
 end
-# pry(main)> vendor.stock(item1, 25)
-#
-# pry(main)> vendor.check_stock(item1)
-# #=> 55
-#
-# pry(main)> vendor.stock(item2, 12)
-#
-# pry(main)> vendor.inventory
-# #=> {#<Item:0x007f9c56740d48...> => 55, #<Item:0x007f9c565c0ce8...> => 12}
