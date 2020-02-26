97cde3a3c428a89522bbdbe0a44edfd2809355b2
Steven Meyers <sjjmeyers@gmail.com>
2020-02-26 11:11:16 -0700

    Pass find who sells test

--- test/market_test.rb
+++ test/market_test.rb
@@ -39,15 +39,16 @@ class MarketTest < Minitest::Test
   end
 
   def test_it_can_show_vendor_names
-    assert_equal ["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"], @market.vendor_names 
+    assert_equal ["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"], @market.vendor_names
+  end
+
+  def test_it_can_find_vendors_that_sell_item
+    assert_equal [@vendor1, @vendor3], @market.vendors_that_sell(@item1)
+    assert_equal [@vendor2], @market.vendors_that_sell(@item4)
   end
 
 end
-# pry(main)> market.vendor_names
-# #=> ["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"]
-#
-# pry(main)> market.vendors_that_sell(item1)
-# #=> [#<Vendor:0x00007fe1348a1160...>, #<Vendor:0x00007fe134910650...>]
+
 #
 # pry(main)> market.vendors_that_sell(item4)
 # #=> [#<Vendor:0x00007fe1349bed40...>]
